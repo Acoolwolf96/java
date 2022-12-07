d765ac46b128f0a101d6b8017c21c431b4c90f4b
Acoolwolf <nelson.njoku@student.hamk.fi>
2022-12-07 12:36:48 +0100

    Updated

--- Build/src/Car.java
+++ Build/src/Car.java
@@ -5,7 +5,7 @@ public class Car {
 	public String model;
 	public int amountOfFuel;
 	
-	
+	//Default Constructors
 	   public Car(){
 	        brand = "";
 	        model = "";
@@ -15,13 +15,13 @@ public class Car {
 	    
 	    }
 
-	    
+	    //Parameterized Constructors
 	    public Car(String brand1, String model1, int amountOfFuel1){
 	        brand = brand1;
 	        model = model1;
 	        amountOfFuel = amountOfFuel1;
 	    }
-	    
+	    //Getters and Setters
 	    public String getModel(){
 	        return model;
 	    }
@@ -34,25 +34,27 @@ public class Car {
 	    	System.out.println("Fuel: " + amountOfFuel);
 	    	
 	    }
-	    
+	    //Refuel method
 	    public void refuel(int amount){
 	        System.out.println("Fuel in the tank: " + amountOfFuel);
 	        System.out.println("Refuel: " + amount);
 	        System.out.println("Fuel in the tank after the refuel: " + (amount + amountOfFuel)); 
 	    }
 
-
+	    //Brake method
 	    public void brake(){
 	        System.out.println("Car is breaking");
 	    }
-
+	    
+	    //Accelerate method
 	    public void accelerate(){
 	        if(amountOfFuel > 0){
 	            System.out.println("Car is accelerating");
 	            amountOfFuel--;
 	        }
 	    }
-
+	    
+	    //Print Data
 	    public void printData(){
 	        System.out.println("Brand: "+ brand);
 	        System.out.println("Model: "+ model);
